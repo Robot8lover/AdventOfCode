dc078f1a12b64bb420af339aac3c8bd5d72ec447
Robot8lover <108429990+Robot8lover@users.noreply.github.com>
2025-12-05 16:06:09 -0800

    update template

---
+++ 2025/00/main-p2.py
@@ -0,0 +1,65 @@
+import sys; sys.dont_write_bytecode = True; from utils import *
+"""
+To do: ensure Code Runner works, have preloaded the day and input in Chrome,
+saved input into the folder, have utils on the side, collapse regions
+
+Strings, lists, dicts:
+lmap, min_max, max_minus_min
+
+Algorithms:
+bisect, binary_search, hamming_distance, edit_distance
+
+Data structures:
+
+List/Vector operations:
+
+Matrices:
+
+Previous problems:
+
+Dict things:
+dict.keys()
+dict.values()
+dict.items()
+"""
+
+def do_case(inp: str, sample=False):
+    # READ THE PROBLEM FROM TOP TO BOTTOM OK
+    def sprint(*a, **k): sample and print(*a, **k)
+    lines = inp.splitlines()
+
+    return # DOES NOTHING, PRINT INSTEAD
+
+run_samples_and_actual([
+# Part 1
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""],[
+# Part 2
+r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+""",r"""
+
+"""], do_case) # day and year can be added as arguments if not doing today
